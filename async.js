0041d5ea5648b215ee235990337a38b392865d9f
banny-anjana <anjanabanny@gmail.com>
2025-01-24 09:37:05 +0530

    Update async.js

--- async.js
+++ async.js
@@ -1,58 +1,63 @@
-//console.log(10);
-//console.log(20);
-//setTimeout(()=>{
-//  console.log(30);
-//}, 5000)
-//console.log(40);
-//console.log(50);
-//setInterval(()=>{
-//  document.writeln("Pari")
-//},1500)
+/*console.log(10);
+console.log(20);
+//setTimeout(()=>{console.log(30)},5000) // console.l0g(30); //? Syntax : setTimeout(()=>{callback} , time)
+console.log(40);
+console.log(50);
+//^ interpreter of java script checks for errors line by line and hence when 
+//^ there is a syntax error encounterd it does'nt move to the next lines of the code
 
-//! Promise
-let p1= new Promise((resolve, reject)=>{});
-console.log(p1)
+/*setInterval(()=>{
+    document.writeln("YOLO") 
+},1500)*/
 
-let p2= new Promise((resolve, reject)=>{
-    resolve("Success");
+//* Promise
+//let p1 = new Promise((resolve , reject)=>{
+//console.log(p1);
+//})
+
+/*let p2 = new Promise((resolve , reject)=>{
+resolve("Success!");
 });
-console.log(p2)
+console.log(p2);
 p2.then((response)=>{
-    console.log(response)
+    console.log(response);
 }).catch((error)=>{
-    console.log(error)
+    console.log(error);
 }).finally(()=>{
-    console.log("finally printing for both!")
-})
+    console.log("Finally is printing for both!");
+})*/
 
-let p3= new Promise((resolve, reject)=>{
-    reject("Failures")
-});
+/*console.log(p2);
+p2.then((response)=>{
+    console.log(response);
+    })*/
+
+/*let p3 = new Promise((resolve , reject)=>{
+reject("Failure!");
+})
 console.log(p3);
-p3.catch((error)=>{
-    console.log(error)
-}).finally(()=>console.log("finally printing for both"))
 
-let p3 = new Promise((resolve, reject) => {
-    reject("Failures");
-});
+/*p3.catch((error)=>{
+    console.log(error);
+})*/
 
-function fetchUsers(){
-let res = window.fetch("https://jsonplaceholder.typicode.com/users")
-//console.log(res);
-res.then(val => {
-    //console.log(val);
-    return val.json().then(data => {
-        console.log(data);
-    });
-}).catch(err => {
-    console.log(err);
-})
-}
-fetchUsers();
+/*p2.finally(()=>{
+    console.log("Finally");
+})*/
+
+
+
+//& for handling the promise we have an instance methods
+//& then(), catch(), finally()
+//& then is used for success and catch is used for failure
+//& catch is used to handle the error
+//& finally is used for both success and failure
+//& finally is used to execute the code after the promise is resolved or rejected
+
+//^method chaining can include both then and catch
 
 // ! Api fetching
-function fetchUsers(){
+/*function fetchUsers(){
 let res = window.fetch("https://jsonplaceholder.typicode.com/users")
 //console.log(res);
 res.then(val => {
@@ -69,6 +74,8 @@ res.then(val => {
                 <td>${user.name}</td>
                 <td>${user.email}</td>
                 <td>${user.company.name}</td>
+                <td>${user.phone}</td>
+                <td>${user.website}</td>
             </tr>
             `
         })
@@ -76,5 +83,32 @@ res.then(val => {
 }).catch(err => {
     console.log(err);
 })
+}
+fetchUsers();*/
+
+/*let p = new Promise((resolve , reject)=>{
+    setTimeout(()=>{
+        resolve("Promise Successfully complteing");
+    }, 3000)
+})
+async function demo(){
+    /*console.log("hello");
+    console.log("hello");
+    console.log("bye");   
+    console.log("10");
+    console.log("20"); 
+    console.log("30");*/
+    /*console.log("Start");
+    let x =await p;
+    console.log(x);
+
+    console.log("End");
+}
+demo();*/
+
+async function fetchUsers(){
+    let response= await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await response.json();
+    console.log(data);
 }
 fetchUsers();
